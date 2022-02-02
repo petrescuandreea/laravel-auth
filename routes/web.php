00dc9f8b84f67e7af8192e44c135806d84920efb
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-02 17:07:20 +0100

    Add logout route in web

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,5 @@ Route::get('/', 'GuestController@home') -> name('home');
 
 Route::post('/login', 'Auth\LoginController@login') -> name('login');
 Route::post('/register', 'Auth\RegisterController@register') -> name('register');
+
+Route::get('/logout', 'Auth\LoginController@logout') -> name('logout');
