cfe300b362e6aad604ebe598b7c867559b841cec
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-02 16:44:50 +0100

    Add login route in web

--- routes/web.php
+++ routes/web.php
@@ -24,4 +24,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'GuestController@home') -> name('home');
 
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
 Route::post('/register', 'Auth\RegisterController@register') -> name('register');
