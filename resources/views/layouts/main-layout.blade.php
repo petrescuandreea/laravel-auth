c610726a43c302a66073cf231170991bb1438d97
Andreea1696 <andreeapetrescu16@gmail.com>
2022-02-04 15:45:53 +0100

    Add snippet for errors after validate form

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -13,6 +13,16 @@
 
     @include('components.header')
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     @yield('content')
 
     @include('components.footer')
